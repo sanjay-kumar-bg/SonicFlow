257f7d70485d283e7b26165dc85c84837be0a0ab
Sanjay <sanjaykumar1379sanjay@gmail,com>
2025-03-09 16:29:51 +0530

    Your commit message describing the changes

---
+++ sonicFlow-BackEnd/src/models/songModel.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const songSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    description: {type: String, required: true},
+    album: {type:String, required: true},
+    image: {type: String, required:true},
+    file: {type:String, required: true}, //music url
+    duration: {type:String,required: true}
+})
+
+const songModel= mongoose.models.song || mongoose.model("song",songSchema); //if song model exist then use it otherwise create song model
+export default songModel;
